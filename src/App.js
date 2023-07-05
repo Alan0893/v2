ac3c641ccd257b7571143043bf75372e0056ab1d
Alan <Alanl07905@gmail.com>
2023-07-04 21:49:06 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from "./pages/Home.js";
+import { Background } from './components/Background';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='app'>
+      <Background>
         <Routes>
           <Route exact path='/' element={<Home />} />
         </Routes>
-      </div>
+      </Background>
     </BrowserRouter>
   );
 }
