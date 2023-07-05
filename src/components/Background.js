35af6dcb26c1927c1bba2d3d34c35ff546b5c9a9
Alan <Alanl07905@gmail.com>
2023-07-04 21:48:09 -0400

    updated background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,62 +1,69 @@
 import React, { useState } from 'react';
 
 export const Background = ({ children }) => {
-	const [spotlightStyle, setSpotlightStyle] = useState(null);
+	const [mouseStyle, setMouseStyle] = useState(null);
 
+	// Event handler for mouse movement
 	const handleMouseMove = (event) => {
+		// Get mouse coordinates and element dimensions
 		const { clientX, clientY } = event;
 		const { left, top, width, height } = event.target.getBoundingClientRect();
+
+		// Calculate spotlight position as a percentage of element dimensions
 		const spotlightX = ((clientX - left) / width) * 100;
 		const spotlightY = ((clientY - top) / height) * 100;
+
+		// Generate the radial gradient color based on spotlight
 		const gradientColor = `radial-gradient(500px circle at ${spotlightX}% ${spotlightY}%, rgba(29, 78, 216, 0.15), transparent 80%)`;
 
-		setSpotlightStyle({ background: gradientColor });
+		// Update the spotlight style state with the gradient color
+		setMouseStyle({ background: gradientColor });
 	};
 
+	// Setting the styling of the components
+	const container = `
+		bg-slate-900
+		leading-relaxed
+		text-slate-400
+		antialiased
+		selection:bg-teal-300
+		selection:text-teal-900
+	`;
+	const mouse = `
+		pointer-events-none 
+		fixed 
+		inset-0 
+		z-30 
+		transition 
+		duration-300 
+		lg:absolute
+	`
+	const position = `
+		mx-auto 
+		min-h-screen 
+		max-w-screen-xl 
+		px-6 py-12 
+		font-sans 
+		md:px-12 
+		md:py-20 
+		lg:px-24 
+		lg:py-0
+	`
+	
+	// Return component
   	return (
 		<div
-			className='
-				bg-slate-900 
-				leading-relaxed 
-				text-slate-400 
-				antialiased 
-				selection:bg-teal-300 
-				selection:text-teal-900
-			'
+			className={container}
 			style={{ minHeight: '100vh' }}
 			onMouseMove={handleMouseMove}
 		>
 			<div className='relative'>
 				<div
-					className='
-						pointer-events-none 
-						fixed 
-						inset-0 
-						z-30 
-						transition 
-						duration-300 
-						lg:absolute
-					'
-					style={spotlightStyle}
+					className={mouse}
+					style={mouseStyle}
 				/>
-				<div className='
-					mx-auto 
-					min-h-screen 
-					max-w-screen-xl 
-					px-6 py-12 
-					font-sans 
-					md:px-12 
-					md:py-20 
-					lg:px-24 
-					lg:py-0
-				'>
-					<div className='
-						lg:flex
-						lg:justify-between
-						lg:gap-4
-					'>
-						{ children }
-					</div>
+				<div className={position}>
+					{ children }
 				</div>
 			</div>
 		</div>
