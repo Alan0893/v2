afedf92697046f27246402163e54714369e97da0
alan <alanl07905@gmail.com>
2024-08-14 15:50:01 -0400

    updated

--- src/components/courses/Junior.js
+++ src/components/courses/Junior.js
@@ -75,6 +75,34 @@ const Junior = () => {
 					</div>
 				</li>
 
+				<li className={Spacing}>
+					<div className={ProjWrapper}>
+						<div className={ProjDiv} />
+						<div className={Time}>
+							Fall
+						</div>
+						<div className={Info}>
+							<h3>
+								<a className={H3Container} href='https://www.bu.edu/academics/cas/courses/cas-cs-350/' target='_blank' rel='noreferrer'>
+									<span className={H3Span} />
+									<span>
+										CS 350 - Distributed Systems
+										<span className='inline-block'>
+											<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={LinkSvg}>
+												<path fillRule='evenodd' d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z' clipRule='evenodd' />
+											</svg>
+										</span>
+									</span>
+								</a>
+							</h3>
+							<p className={InfoP}>
+								Programming-centric introduction to computer systems. The course discusses system design principles, performance analysis, communication and synchronization primitives, concurrency control, database transactions, data consistency, task and data parallelism, replication, fault tolerance, and distributed consensus. 
+							</p>
+							
+						</div>
+					</div>
+				</li>
+
 			</ul>
 		</div>
   	)
--- src/components/projects/2023.js
+++ src/components/projects/2023.js
@@ -21,7 +21,63 @@ const Projects23 = () => {
 		<div>
 			<ul className={ProjContainer}>
 
-			<li className={Spacing}>
+				<li className={Spacing}>
+					<div className={ProjWrapper}>
+						<div className={ProjDiv} />
+						<div className={Time}>
+							October - December
+						</div>
+						<div className={Info}>
+							<h3>
+								<a className={H3Container} href='https://github.com/Alan0893/CS320-2023-Fall-alan/tree/main/assigns/interp3' target='_blank' rel='noreferrer'>
+									<span className={H3Span} />
+									<span>
+										<img
+											className='mr-2 h-5 w-5 inline'
+											src='https://raw.githubusercontent.com/ocaml/ocaml-logo/master/Colour/PNG/colour-transparent-icon.png'
+											alt='favicon'
+										/>
+										OCaml Interpreter
+										<span className='inline-block'>
+											<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={LinkSvg}>
+												<path fillRule='evenodd' d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z' clipRule='evenodd' />
+											</svg>
+										</span>
+									</span>
+								</a>
+							</h3>
+							<p className={InfoP}>
+								Designed and documented 15+ evaluation rules for function application, sequence expressions, conditional expressions, and trace expressions.
+								Compiled high-level constructs into stack commands, maintaining execution invariants across all test cases and optimizing sequences (Push, Trace, Pop, Swap) for efficiency.
+								Validated the language's behavior through the design and interpretation of sample programs, ensuring expected trace outputs and correct handling of edge cases.
+								Capitalized the TMDB API to facilitate targeted searched for specific movies, series, or actor queries, enhancing user search experience by 30%.
+							</p>
+							<ul className={ChipWrapper}>
+								<li className='mr-1.5 mt-2'>
+									<div className={Chip}>OCaml</div>
+								</li>
+								<li className='mr-1.5 mt-2'>
+									<div className={Chip}>Formal Semantics</div>
+								</li>
+								<li className='mr-1.5 mt-2'>
+									<div className={Chip}>Grammar</div>
+								</li>
+							</ul>
+						</div>
+						<img 
+							alt='interpreter' 
+							loading='lazy' 
+							width='175' 
+							height='109' 
+							decoding='async'
+							className={Image} 
+							style={{ color: 'transparent' }} 
+							src='/assets/projects/interp.svg'
+						/>
+					</div>
+				</li>
+
+				<li className={Spacing}>
 					<div className={ProjWrapper}>
 						<div className={ProjDiv} />
 						<div className={Time}>
